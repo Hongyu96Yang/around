82c81ac023b669215ee037688586e812ff3bf4b9
Hongyu <hongyuy96@gmail.com>
2022-02-17 20:48:17 +0000

    elastic search and gcs with search and save function

---
+++ constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+// constants of a users
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.182.0.2:9200"
+    ES_USERNAME = "hongyuy96"
+    ES_PASSWORD = "486900"
+    GCS_BUCKET = "hongyuy"
+
+)
